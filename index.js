552750e500d7636009310ce1506ba0d71ae7a7cc
unknown <utkarshmaheria@gmail.com>
2020-08-27 00:14:08 +0530

    added the create task function to store values in db

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const express = require('express');
 const port = 8000;
 
+
+const db = require('./config/mongoose');
+const Todolist = require('./models/todolist');
+
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.urlencoded());
+app.use(express.static('assets'));
+
+app.get('/', function(req, res){
+
+    Todolist.find({}, function(err, tasks){
+        if(err)
+        {
+            console.log('Error in retrieving the data from database');
+            return;
+        }
+
+        return res.render('todo', 
+        { 
+            title: 'To Do List' , 
+            todo_list: tasks 
+        });
+    });
+});
+   
+
+app.post('/create-task', function(req, res){
+
+    Todolist.create({
+        description: req.body.description,
+        category: req.body.category,
+        date: req.body.date
+    }, function(err, newTask){
+        if(err)
+        { 
+            console.log('error in creating a task'); 
+            return; 
+        }
+
+        console.log('************', newTask);
+
+        return res.redirect('/');
+    });
+
+});
 
 app.listen(port, function(err){
     if(err)
